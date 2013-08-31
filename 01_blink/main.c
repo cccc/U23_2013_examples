f0701cdcd77900122c1d476b2b8aa50466133f4a
Florian Zeitz <florob@babelmonkeys.de>
2013-08-31 22:29:01 +0200

    Coding style in 'main.c's

--- 01_blink/main.c
+++ 01_blink/main.c
@@ -12,13 +12,14 @@ int main()
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 	//Initialize GPIOs for LEDs
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &(GPIO_InitTypeDef){
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_OType = GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_UP,
+		.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13
+		            | GPIO_Pin_14 | GPIO_Pin_15
+	});
 
 	while (1)
 	{
--- 02_systick/main.c
+++ 02_systick/main.c
@@ -9,7 +9,7 @@ void SysTick_Handler()
 	SysTickCounter++;
 }
 
-//Proper delay function based on the SysTick counter
+// Proper delay function based on the SysTick counter
 void Delay(uint32_t milliseconds)
 {
 	volatile uint32_t end=SysTickCounter+milliseconds;
@@ -19,29 +19,31 @@ void Delay(uint32_t milliseconds)
 
 int main()
 {
-	//Get information about the configured clock frequencies
+	// Get information about the configured clock frequencies
 	RCC_ClocksTypeDef RCC_Clocks;
 	RCC_GetClocksFreq(&RCC_Clocks);
 
-	//Set the tick rate of the SysTick timer to 1ms
-	//RCC_Clocks.HCLK_Frequency contains the amount of ticks of the counter per second
-	//divided by 1000 we get the amount of ticks per millisecond
+	// Set the tick rate of the SysTick timer to 1ms
+	// RCC_Clocks.HCLK_Frequency contains the amount of ticks of the counter
+	// per second divided by 1000 we get the amount of ticks per millisecond
 	SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
 
-	//Configure the interrupt controller to have 16 different priorities and no subpriorities
+	// Configure the interrupt controller to have 16 different priorities
+	// and no subpriorities
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 
-	//Enable clock for GPIOD peripheral
+	// Enable clock for GPIOD peripheral
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	//Initialize GPIOs for LEDs
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	// Initialize GPIOs for LEDs
+	GPIO_Init(GPIOD, &(GPIO_InitTypeDef){
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_OType = GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_UP,
+		.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13
+		            | GPIO_Pin_14 | GPIO_Pin_15
+	});
 
 	while (1)
 	{
